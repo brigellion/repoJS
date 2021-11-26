4677c3923a3268a277d247cb2ce20c7676c29428
Andrey Poymanov <poym4nov@yandex.ru>
2021-11-26 20:24:25 +0300

    complete

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    title,
-    adaptive;
-alert('Привет!');
-console.log('Я скрипт');
+let screens = 'Простые, Сложные, Интерактивные',
+    screenPrice = 200,
+    rollback = 30,
+    fullPrice = 5000,
+    title = 'Project',
+    adaptive = false;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+
+screens = screens.toLowerCase();
+console.log(screens.split(', '));
+
+console.log(fullPrice * (rollback / 100));
