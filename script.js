315a85404b8713d87d4c8dfc0081efa4d254ddf9
Andrey Poymanov <poym4nov@yandex.ru>
2021-11-29 23:02:06 +0300

    edit

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 'use strict';
 
-let screens = 'Простые, Сложные, Интерактивные',
-    screenPrice = 1000,
+let screenPrice = 1000,
     rollback = 30,
-    fullPrice = 5000,
-    title = 'Project',
-    adaptive = false;
+    title = prompt('Как называется ваш проект?'),
+    screens = prompt('Какие типы экранов нужно разработать?'),
+    adaptive = !!prompt('Нужен ли адаптив на сайте?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?'),
+    servicePrice1 = +prompt('Сколько это будет стоить?'),
+    service2 = prompt('Какой еще дополнительный тип услуги нужен?'),
+    servicePrice2 = +prompt('Скольqweко это будет стоить?'),
+    fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 
 console.log(typeof title);
@@ -21,17 +26,6 @@ screens = screens.toLowerCase();
 console.log(screens.split(', '));
 
 console.log(fullPrice * (rollback / 100));
-
-title = prompt('Как называется ваш проект?');
-screens = prompt('Какие типы экранов нужно разработать?');
-adaptive = !!prompt('Нужен ли адаптив на сайте?');
-
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой еще дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Скольqweко это будет стоить?');
-
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 let servicePercentPrice = Math.round(fullPrice - (fullPrice * (rollback / 100)));
 console.log(servicePercentPrice);
